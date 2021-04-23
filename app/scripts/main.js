8eea00487c63a237d8753bddd4263dfbd24e99b7
Nick <55245151+nickrepenkoff@users.noreply.github.com>
2021-04-23 03:58:23 +0300

    Add files via upload

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,13 @@
-$(document).click(function(event) { 
-    var $target = $(event.target);
-    if(!$target.closest('#historywrap').length && 
-    $('#historymenu').is(':visible')) {
-      $('#historymenu').hide();
-    }        
-  });
+// $(document).click(function(event) { 
+//   var $target = $(event.target);
+//   if(!$target.closest('#historywrap').length && 
+//   $('#historymenu').is(':visible')) {
+//     $('#historymenu').hide();
+//   }        
+// });
 
 $('#historybtn').click(function(){
-    $('#historymenu').toggle()
+    $('#historymenu').toggle();
 });
+
+
